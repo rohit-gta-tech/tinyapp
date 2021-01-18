76468ad20090e8bfdc4a51007e0cad47050fdfde
Rohit <rohitcricbuzz@gmail.com>
2021-01-18 20:17:48 +0000

    URL show page and added and route added in server

--- express_server.js
+++ express_server.js
@@ -20,21 +20,17 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//GET call to show list or index of all URLs
 app.get("/urls", (req,res) => {
-  const urlList = {urls: urlDatabase};
-  console.log(urlList);
-  res.render("urls_index", urlList);
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
 });
 
-// example to show variable a is locally scoped and is not accessible by other get calls
-/*app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-   
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});*/
+//GET call to show a particular URL and its short name by passing its short name as request parameter
+app.get("/urls/:shortURL", (req,res) => {
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+    res.render("urls_show", templateVars);
+  });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
