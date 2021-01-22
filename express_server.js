5fa3e51a7fefae6ee0d898f9e222ff56439b0d12
Rohit <rohitcricbuzz@gmail.com>
2021-01-22 02:05:19 +0000

    Error handling due to [ERR_HTTP_HEADERS_SENT] rectified

--- express_server.js
+++ express_server.js
@@ -28,10 +28,11 @@ const users = {};
 
 //rendering homepage
 app.get("/", (req, res) => {
-  if (!req.session.user_id) {
+  if (req.session.user_id) {
+    res.redirect('/urls');
+  } else {
     res.redirect('/login');
   }
-  res.redirect('/urls');
 });
 
 //GET call to show list or index of all URLs
@@ -42,11 +43,12 @@ app.get("/urls", (req,res) => {
 
 //GET route to render the new urls_new template
 app.get("/urls/new", (req, res) => {
-  if (!req.session.user_id) {
+  if (req.session.user_id) {
+    const templateVars = {user: users[req.session.user_id]};
+    res.render("urls_new", templateVars);
+  } else {
     res.redirect("/login");
   }
-  const templateVars = {user: users[req.session.user_id]};
-  res.render("urls_new", templateVars);
 });
 
 //GET call to show a particular URL and its short name by passing its short name as request parameter
@@ -94,8 +96,9 @@ app.post("/urls/:shortURL", (req, res) => {
     } else {
       res.render("url_errors", {user: req.session.user_id, error: "No such tinyURL!"});
     }
+  } else {
+    res.render("url_errors", {user: null, error: "You are not authorized!"});
   }
-  res.render("url_errors", {user: null, error: "You are not authorized!"});
 });
 
 // POST route for deleting URLs
@@ -107,45 +110,45 @@ app.post("/urls/:shortURL/delete", (req, res) => {
     } else {
       res.render("url_errors", {user: req.session.user_id, error: "No such tinyURL!"});
     }
+  } else {
+    res.render("url_errors", {user: null, error: "You are not authorized!"});
   }
-  res.render("url_errors", {user: null, error: "You are not authorized!"});
 });
 
 //GET route for user login page
 app.get("/login", (req, res) => {
   if (req.session.user_id) {
     res.redirect('/urls');
+  } else {
+    const templateVars = {user: null};
+    res.render("user_login", templateVars);
   }
-  const templateVars = {user: null};
-  res.render("user_login", templateVars);
-  
 });
 
 //GET route for user registration landing page
 app.get("/register", (req, res) => {
   if (req.session.user_id) {
     res.redirect('/urls');
+  } else {
+    const templateVars = {user: null};
+    res.render("user_register", templateVars);
   }
-  const templateVars = {user: null};
-  res.render("user_register", templateVars);
-  
 });
 
 //POST route for login
 app.post("/login", (req, res) => {
   const incomingEmail = req.body.email;
   const incomingPassword = req.body.password;
-
-  if (!getUserByEmail(users, incomingEmail)) {
-    res.status(403).render("url_errors", {user: null, error: "Sorry, it seems you are not registered. Please go to the registration page!"});
-  }
-
   const user = getUserByEmail(users, incomingEmail);
-  if (!bcrypt.compareSync(incomingPassword, user.password)) {
+
+  if (!user) {
+    res.status(403).render("url_errors", {user: null, error: "Sorry, it seems you are not registered. Please go to the registration page!"});
+  } else if (!bcrypt.compareSync(incomingPassword, user.password)) {
     res.status(403).render("url_errors", {user: null, error: "Sorry, the entered password is incorrect. Please try again!"});
+  } else {
+    req.session.user_id = user.id;
+    res.redirect('/urls');
   }
-  req.session.user_id = user.id;
-  res.redirect('/urls');
 });
 
 //POST route for user registration
