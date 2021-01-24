3d625abfff386b5fa25c034ce00c8893eb3ea21e
Rohit <rohitcricbuzz@gmail.com>
2021-01-24 00:15:10 +0000

    RESTful routes and analytics incorporated

--- express_server.js
+++ express_server.js
@@ -13,12 +13,20 @@ const bcrypt = require('bcrypt');
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Middleware cookie-parser
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
 //Middleware cookie-session
 const cookieSession = require('cookie-session');
 app.use(cookieSession({
   name: 'session',
   keys: ['user_id']
 }));
+
+//Middleware method override
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 //original URL database
 const urlDatabase = {};
@@ -55,9 +63,12 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req,res) => {
   if (!req.session.user_id) {
     res.status(400).render("urls_show", {user: null, error: "No such tinyURL exists! Please login or register!"});
-  } else {
-    if (urlDatabase[req.params.shortURL].userID === req.session.user_id) {
-      const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.session.user_id], error: null};
+  } 
+  if (req.session.user_id) {
+    if(!urlDatabase[req.params.shortURL]) {
+      res.status(400).render("urls_show", {user: users[req.session.user_id], error: "No such TinyURL exists!"});
+    } else if (urlDatabase[req.params.shortURL].userID === req.session.user_id) {
+      const templateVars = { url: urlDatabase[req.params.shortURL], user: users[req.session.user_id], error: null};
       res.render("urls_show", templateVars);
     } else {
       res.status(400).render("urls_show", {user: users[req.session.user_id], error: "You have not created any such short URL!"});
@@ -68,7 +79,12 @@ app.get("/urls/:shortURL", (req,res) => {
 // Redirect any request to "/u/:shortURL" to its longURL
 app.get("/u/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL]) {
+    urlDatabase[req.params.shortURL].visits.push({date: new Date(), vid: generateRandomString()})
     const longURL = urlDatabase[req.params.shortURL].longURL;
+    if(!req.cookies[req.params.shortURL]) {
+      res.cookie(req.params.shortURL, generateRandomString());
+      urlDatabase[req.params.shortURL].uniqueVisits++;
+    };
     res.redirect(longURL);
   } else {
     res.render("url_errors", {user: users[req.session.user_id], error: "Not a valid tiny URL"});
@@ -79,7 +95,7 @@ app.get("/u/:shortURL", (req, res) => {
 app.post("/urls", (req, res) => {
   if (req.session.user_id) {
     let newId = generateRandomString();
-    urlDatabase[newId] = {longURL: req.body.longURL, userID: req.session.user_id};     //adds the new URL to our urlDatabase object
+    urlDatabase[newId] = {shortURL: newId, longURL: req.body.longURL, userID: req.session.user_id, dateCreated: new Date(), visits: [], uniqueVisits: 0};     //adds the new URL to our urlDatabase object
     res.redirect(`/urls/${newId}`);     //Redirected to the newly submitted URL
   } else {
     res.render("url_errors", {user: null, error: "You are not authorized!"});
@@ -87,10 +103,10 @@ app.post("/urls", (req, res) => {
 });
 
 //POST route for updating URLs
-app.post("/urls/:shortURL", (req, res) => {
+app.put("/urls/:shortURL", (req, res) => {
   if (req.session.user_id) {
     if (urlDatabase[req.params.shortURL].userID === req.session.user_id) {
-      urlDatabase[req.params.shortURL] = { longURL: req.body.longURL, userID: req.session.user_id };
+      urlDatabase[req.params.shortURL].longURL = req.body.longURL;
       res.redirect('/urls');
     } else {
       res.render("url_errors", {user: req.session.user_id, error: "No such tinyURL!"});
@@ -101,7 +117,7 @@ app.post("/urls/:shortURL", (req, res) => {
 });
 
 // POST route for deleting URLs
-app.post("/urls/:shortURL/delete", (req, res) => {
+app.delete("/urls/:shortURL/delete", (req, res) => {
   if (req.session.user_id) {
     if (urlDatabase[req.params.shortURL].userID === req.session.user_id) {
       delete urlDatabase[req.params.shortURL];
@@ -140,7 +156,9 @@ app.post("/login", (req, res) => {
   const incomingPassword = req.body.password;
   const user = getUserByEmail(users, incomingEmail);
 
-  if (!user) {
+  if(!incomingEmail || !incomingPassword) {
+    res.status(403).render("url_errors", {user: null, error: "Please enter both email-id and password to login"});
+  } else if (!user) {
     res.status(403).render("url_errors", {user: null, error: "Sorry, it seems you are not registered. Please go to the registration page!"});
   } else if (!bcrypt.compareSync(incomingPassword, user.password)) {
     res.status(403).render("url_errors", {user: null, error: "Sorry, the entered password is incorrect. Please try again!"});
